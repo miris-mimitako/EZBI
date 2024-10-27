6b82d5346c638394e5f4757b89e469944c23357c
miris-mimitako <miris.mimitako@gmail.com>
2024-10-27 15:44:46 +0900

    trial pages

--- webapps/mod/views.py
+++ webapps/mod/views.py
@@ -1,5 +1,17 @@
+from flask import render_template
+
 from mod import app
 
 @app.route('/')
-def index():
+def test():
     return 'Hellow World!'
+
+@app.route("/indexhtml")
+def index():
+    return render_template("index.html", insert_something = "del")
+
+@app.route("/page2")
+def page2():
+    contents = {"content1": "content1",
+                "content2": "content2"}
+    return render_template("page2.html", insert_something = contents)
